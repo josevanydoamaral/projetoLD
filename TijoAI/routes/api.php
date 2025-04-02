a8115e3ed298e0b175329dedb1b10a237f46652c
Josevany do Amaral <josevanydoamaral@gmail.com>
2025-04-02 16:31:36 +0100

    dreamweaver controller and routes

--- TijoAI/routes/api.php
+++ TijoAI/routes/api.php
@@ -1,8 +1,20 @@
 <?php
 
+use App\Http\Controllers\DreamWeaverController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+
+
+
+// DREAMWEAVER ROUTES
+Route::post('image/generate', [DreamWeaverController::class, 'generate']);
+Route::post('image/zoom/in', [DreamWeaverController::class, 'zoomIn']);
+Route::post('image/zoom/out', [DreamWeaverController::class, 'zoomOut']);
+Route::get('/status/{job_id}', [DreamWeaverController::class, 'status']);
+Route::get('/result/{job_id}', [DreamWeaverController::class, 'result']);
